cc0dcb75fcfe5872eb42a9dcb65aec28e6ee5b1c
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-21 19:27:35 -0600

    finished

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Celcius to Fahrenheit! (49)");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(49.0));
+
+            Console.WriteLine("Fahrenheit to Celcius! (49)");
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(49.0));
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+
+        }
+
+        public static double FahrenheitToCelsius(double input)
+        {
+            return (input - 32.0) * 5.0/9.0;
+        }
+
+        public static double CelsiusToFahrenheit(double input)
+        {
+            return (input * 9.0/5.0) + 32.0;
+        }
+    }
+}
